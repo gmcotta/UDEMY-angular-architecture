6879d38998ef9ad858dc2f378f494091f321706f
Gustavo Cotta <gmcotta34@gmail.com>
2020-12-14 18:57:02 -0300

    Adding sign out feature on header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 
 import * as fromRoot from './store';
 import * as fromUser from './store/user';
 import * as fromDictionaries from './store/dictionaries';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'aa-root',
@@ -12,12 +13,21 @@ import * as fromDictionaries from './store/dictionaries';
 })
 export class AppComponent implements OnInit {
 
+  isAuthorized$ = new Observable<boolean | null>();
+
   constructor(
     private store: Store<fromRoot.State>
   ) {}
 
   ngOnInit(): void {
+    this.isAuthorized$ = this.store.pipe(
+      select(fromUser.getIsAuthorized)
+    );
     this.store.dispatch(new fromUser.Init());
     this.store.dispatch(new fromDictionaries.Read());
+  }
+
+  onSignOut(): void {
+    this.store.dispatch(new fromUser.SignOut());
   }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  ChangeDetectionStrategy, 
+  Component, 
+  Input, 
+  OnInit, 
+  Output, 
+  EventEmitter 
+} from '@angular/core';
 
 @Component({
   selector: 'aa-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent implements OnInit {
+  @Input() isAuthorized: boolean | null = false;
+  @Output() signOut = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSignOut(): void {
+    this.signOut.emit();
+  }
 }

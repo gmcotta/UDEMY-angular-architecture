0abfe825153b8f6108cd323478d0efcf6dfad6fe
Gustavo Cotta <gmcotta34@gmail.com>
2020-12-14 13:33:15 -0300

    Adding user reducer to store

--- src/app/store/user/user.models.ts
+++ src/app/store/user/user.models.ts
@@ -1,4 +1,4 @@
-import { User, Recruiter, Employee } from '@app/models/backend/user';
+export { User, Recruiter, Employee } from '@app/models/backend/user';
 
 export interface EmailPasswordCredentials {
   email: string;
---
+++ src/app/store/user/user.reducer.ts
@@ -0,0 +1,85 @@
+import { User } from '@app/models/backend';
+import * as fromActions from './user.actions';
+import {} from './user.models';
+
+export interface UserState {
+  entity: User | null;
+  uid: string | null;
+  loading: boolean | null;
+  error: string | null;
+}
+
+const initialState: UserState = {
+  entity: null,
+  uid: null,
+  loading: null,
+  error: null,
+}
+
+export function reducer(
+  state = initialState, 
+  action: fromActions.All
+): UserState {
+  switch (action.type) {
+    case fromActions.Types.SIGN_IN_EMAIL: {
+      return { 
+        ...state, 
+        loading: true, 
+      };
+    };
+    case fromActions.Types.SIGN_IN_EMAIL_SUCCESS: {
+      return { 
+        ...state, 
+        entity: action.user, 
+        uid: action.uid,
+        loading: false,
+      };
+    };
+    case fromActions.Types.SIGN_IN_EMAIL_ERROR: {
+      return { 
+        ...state, 
+        error: action.error, 
+        loading: false, 
+      };
+    };
+    case fromActions.Types.SIGN_UP_EMAIL: {
+      return { 
+        ...state, 
+        loading: true, 
+      };
+    };
+    case fromActions.Types.SIGN_UP_EMAIL_SUCCESS: {
+      return { 
+        ...state,
+        uid: action.uid,
+        loading: false,
+      };
+    };
+    case fromActions.Types.SIGN_UP_EMAIL_ERROR: {
+      return { 
+        ...state,
+        error: action.error,
+        loading: false,
+      };
+    };
+    case fromActions.Types.SIGN_OUT: {
+      return { 
+        ...state, 
+        loading: true, 
+      };
+    };
+    case fromActions.Types.SIGN_OUT_SUCCESS: {
+      return { 
+        ...initialState
+      };
+    };
+    case fromActions.Types.SIGN_OUT_ERROR: {
+      return { 
+        ...state, 
+        loading: false,
+        error: action.error,
+      };
+    };
+    default: return { ...state };
+  }
+}

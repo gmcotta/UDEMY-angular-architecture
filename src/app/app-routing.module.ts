c86bf73d431a481a1f2f4bb946737545daa82faa
Gustavo Cotta <gmcotta34@gmail.com>
2020-12-14 16:29:31 -0300

    Adding auth page links on header and linking auth module on app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,13 @@ const routes: Routes = [
     children: [
       {
         path: 'demo',
-        loadChildren: () => import('./pages/demo/demo.module').then(m => m.DemoModule),
+        loadChildren: () => import('./pages/demo/demo.module')
+          .then(m => m.DemoModule),
+      },
+      {
+        path: 'auth',
+        loadChildren: () => import('./pages/auth/auth.module')
+          .then(m => m.AuthModule),
       }
     ]
   }

a027c7443be6fa19aa247979bab237d76b531232
Gustavo Cotta <gmcotta34@gmail.com>
2020-12-15 13:43:21 -0300

    Changing form template and adding stepper component and service

---
+++ src/app/pages/profile/pages/form/components/stepper/stepper.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StepperComponent } from './stepper.component';
+import { StepperService } from './services/stepper.service';
+
+
+@NgModule({
+  declarations: [
+    StepperComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    StepperComponent,
+  ],
+  providers: [
+    StepperService,
+  ],
+})
+export class StepperModule { }

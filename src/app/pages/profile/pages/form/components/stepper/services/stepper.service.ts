a719f1fe927412129619ca91fe8d4ffaf036bc15
Gustavo Cotta <gmcotta34@gmail.com>
2020-12-15 13:57:26 -0300

    Working on stepper template, component and service

--- src/app/pages/profile/pages/form/components/stepper/services/stepper.service.ts
+++ src/app/pages/profile/pages/form/components/stepper/services/stepper.service.ts
@@ -1,7 +1,24 @@
 import { Injectable } from '@angular/core';
 
+export interface Step {
+  key: string;
+  label: string;
+}
+
+export interface ActiveStep extends Step {
+  index: number;
+}
+
 @Injectable()
 export class StepperService {
 
+  steps: Step[] = [];
+  activeStep: ActiveStep = {} as ActiveStep;
+
   constructor() { }
+
+  init(steps: Step[]): void {
+    this.steps = steps;
+    this.activeStep = { ...steps[0], index: 0 };
+  }
 }

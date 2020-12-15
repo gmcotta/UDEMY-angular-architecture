f8611e0575b5c9c6ce52371a95899f4625af8d6b
Gustavo Cotta <gmcotta34@gmail.com>
2020-12-14 22:01:28 -0300

    Adding onDrop method on files-upload component

--- src/app/shared/popups/files-upload/files-upload.component.ts
+++ src/app/shared/popups/files-upload/files-upload.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+export interface DialogData {
+  multiple: boolean;
+  crop: boolean;
+}
 @Component({
   selector: 'aa-files-upload',
   templateUrl: './files-upload.component.html',
@@ -9,13 +14,40 @@ export class FilesUploadComponent implements OnInit {
 
   isHovering: boolean = false;
 
-  constructor() { }
+  files: Array<File | null> = [];
+  imageFile = new File([], '');
+  isError = false;
+  filesURLs: string[] = [];
+
+  constructor(
+    private dialogRef: MatDialogRef<FilesUploadComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+  ) { }
 
   ngOnInit(): void {
   }
 
   toggleHover(event: boolean) {
     this.isHovering = event;
+  }
+
+  onDrop($event: Event): void {
+    const element = $event.target as HTMLInputElement;
+    const files = element.files;
+    this.isError = false;
+
+    if (files) {
+      if (this.data.crop && files?.length > 1) {
+        this.isError = true;
+        return;
+      }
+
+      for (let i = 0; i < files?.length; i++) {
+        this.files.push(files.item(i));
+      }
+
+    }
+
   }
 
 }
